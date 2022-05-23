e9d24394d286fe5a8c47378288a1ce8e7a2a05b2
gurjais <gurjais@ipvc.pt>
2022-05-23 10:58:01 +0100

    commit1

--- Android/app/src/main/java/com/example/libsked/Appointments.kt
+++ Android/app/src/main/java/com/example/libsked/Appointments.kt
@@ -3,7 +3,7 @@ package com.example.libsked
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Appointments : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_appointments)
