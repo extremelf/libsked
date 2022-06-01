500e013fb37cc1f10ad3660ee6b1d85218238c27
HSimon17 <67068646+HSimon17@users.noreply.github.com>
2022-06-01 18:01:17 +0100

    Lógica ForgotPassword.kt criada

---
+++ Android/app/src/main/java/com/example/libsked/authentication/ForgotPassword.kt
@@ -0,0 +1,54 @@
+package com.example.libsked.authentication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.libsked.R
+import com.google.firebase.auth.FirebaseAuth
+
+class forgotPassword : AppCompatActivity() {
+
+    lateinit var Email: EditText
+    private lateinit var btnReset: Button
+    private lateinit var btnLogin: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_password)
+
+        // Ligar às Views do XML
+        Email = findViewById(R.id.et_email)
+        btnReset = findViewById(R.id.bt_continue)
+        btnLogin = findViewById(R.id.bt_signup)
+
+        btnReset.setOnClickListener {
+
+            val email = Email.text.toString()
+
+            if (email. isEmpty()){
+                Toast.makeText(this, "Please Enter Email Address!", Toast.LENGTH_SHORT).show()
+            }else{
+                FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+                    .addOnCompleteListener{task ->
+                        if(task.isSuccessful){
+                            Toast.makeText(this, "Email Sent Successfully To Reset Your Password! ", Toast.LENGTH_LONG).show()
+                            val intent = Intent(this, Login::class.java)
+                            startActivity(intent)
+                            finish()
+                        }else{
+                            Toast.makeText(this, task.exception!!.message.toString(), Toast.LENGTH_LONG).show()
+                        }
+                    }
+                }
+            }
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
