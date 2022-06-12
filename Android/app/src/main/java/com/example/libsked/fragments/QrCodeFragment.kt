b18f52cb49df42837350283f84ecded8a5ef1f4b
HSimon17 <67068646+HSimon17@users.noreply.github.com>
2022-06-13 00:53:24 +0100

    QrCode Implemented

--- Android/app/src/main/java/com/example/libsked/fragments/QrCodeFragment.kt
+++ Android/app/src/main/java/com/example/libsked/fragments/QrCodeFragment.kt
@@ -1,13 +1,22 @@
 package com.example.libsked.fragments
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.AutoFocusMode
 import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.CodeScannerView
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
 import com.example.libsked.R
+import kotlinx.android.synthetic.main.fragment_qr_code.*
 import java.util.jar.Manifest
 
 // TODO: Rename parameter arguments, choose names that match
@@ -27,12 +36,14 @@ class QrCodeFragment : Fragment() {
 
     private lateinit var codeScanner: CodeScanner
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
     }
 
     override fun onCreateView(
@@ -42,6 +53,73 @@ class QrCodeFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_qr_code, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if(ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.CAMERA) ==
+            PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(android.Manifest.permission.CAMERA), 123)
+        }else{
+            startScanning()
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (::codeScanner.isInitialized){
+            codeScanner?.startPreview()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (::codeScanner.isInitialized){
+            codeScanner?.releaseResources()
+        }
+    }
+
+    private fun startScanning(){
+
+        codeScanner = CodeScanner(requireContext(), scanner_view)
+        codeScanner.camera = CodeScanner.CAMERA_BACK
+        codeScanner.formats = CodeScanner.ALL_FORMATS
+
+        codeScanner.autoFocusMode = AutoFocusMode.SAFE
+        codeScanner.scanMode = ScanMode.SINGLE
+        codeScanner.isAutoFocusEnabled = true
+        codeScanner.isFlashEnabled = false
+
+        codeScanner.decodeCallback = DecodeCallback {
+            requireActivity().runOnUiThread {
+                Toast.makeText(requireContext(), "Scan Result: ${it.text}", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        codeScanner.errorCallback = ErrorCallback {
+            requireActivity().runOnUiThread {
+                Toast.makeText(requireContext(), "Camera Initialization error: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        scanner_view.setOnClickListener {
+            codeScanner.startPreview()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 123){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(requireContext(), "Camera Permission Granted!", Toast.LENGTH_SHORT).show()
+                startScanning()
+            }else{
+                Toast.makeText(requireContext(), "Camera Permission Denied!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
 
     companion object {
         /**
@@ -61,5 +139,5 @@ class QrCodeFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
-    }
+        }
 }
